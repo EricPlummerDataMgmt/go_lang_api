a3a59176bcfdb1f6f960c043414ea9bd2e877d1d
EricPlummerDataMgmt <eric.plummer@datamgmt.co>
2026-08-05 23:47:11 -0400

    Updated  Main.go

--- cmd/go_lang_api/main.go
+++ cmd/go_lang_api/main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-}
+
+	router := routes.NewRouter()
+
+	port := 8080
+	addr :- fmt.Sprintf(":%d", port)
+
+	fmt.Printf("Server Listening on http://localhost%s...\n", addr)
+	err := http.ListenAndServe(addr, router)
+	if err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
