ccdca519533d05a372e12a5d106b18a01ce85e3a
EricPlummerDataMgmt <eric.plummer@datamgmt.co>
2026-08-05 23:38:22 -0400

    createdd Router code

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -8,5 +8,13 @@ import (
 func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc(("/", indexHandler)
-)
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/api/data", apiDataHander)
+
+	return mux
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintln((w, "Welcome to the Home Page"))
+	
